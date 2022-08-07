9f3a87fdee1fca81775849f94fe48a93f5a60e84
Eben Eleazer <ebenaeleazer@outlook.com>
2022-08-06 21:59:22 -0400

    Create slice for users Creates slice for user to be stored in redux store

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+  },
+
+  reducers: {
+
+    // login: (state, action) => {
+    //   state.user = action.payload
+    // },
+    
+  }
+
+})
+
+// Action creators are generated for each case reducer function
+export const { login } = userSlice.actions
+
+export default userSlice.reducer
